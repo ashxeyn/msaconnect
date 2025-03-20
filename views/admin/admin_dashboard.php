40fcbddf127aa6664451c4bff7c4d8b37f9374f8
ashxeyn <ashxeyn@gmail.com>
2025-03-20 21:33:30 +0800

    Added session for relogin

--- views/admin/admin_dashboard.php
+++ views/admin/admin_dashboard.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php include "../../accounts/session.php"; ?>
     <?php include '../../includes/head.php'; ?> 
     <title>Admin Dashboard</title>    
     <link rel="stylesheet" href="../../css/sideBar.css">
